ead01ad611639a46af8c2eefda178bd8660deccb
Lars Rustand <rustand.lars@gmail.com>
2024-02-04 21:00:33 +0100

    Refactor keycap width and height defaults

--- src/my_project/core.clj
+++ src/my_project/core.clj
@@ -177,6 +177,46 @@
   (nth [-10 -3 6] row))
 
 
+(defn top-width
+  "Return `top-width` for given U."
+  [size]
+  (case size
+    :1u 12
+    :1.5u 20
+    :2u 35
+    :2uh 12))
+
+
+(defn bottom-width
+  "Return `bottom-width` for given U."
+  [size]
+  (case size
+    :1u 16
+    :1.5u 24
+    :2u 35
+    :2uh 16))
+
+
+(defn bottom-height
+  "Return `bottom-height` for given U."
+  [size]
+  (case size
+    :1u 16
+    :1.5u 16
+    :2u 16
+    :2uh 35))
+
+
+(defn top-height
+  "Return `top-height` for given U."
+  [size]
+  (case size
+    :1u 12
+    :1.5u 12
+    :2u 12
+    :2uh 30))
+
+
 ;; TODO Make actual keycap profile
 (defn keycap
   "Generate a keycap of the given size and row."
@@ -190,26 +230,10 @@
       :or {row 1
            size :1u
            angle (keycap-angle row)
-           bottom-width (case size
-                          :1u 16
-                          :1.5u 24
-                          :2u 35
-                          :2uh 16)
-           bottom-height (case size
-                           :1u 16
-                           :1.5u 16
-                           :2u 16
-                           :2uh 35)
-           top-width (case size
-                       :1u 12
-                       :1.5u 20
-                       :2u 35
-                       :2uh 12)
-           top-height (case size
-                        :1u 12
-                        :1.5u 12
-                        :2u 12
-                        :2uh 30)}}]
+           bottom-width (bottom-width size)
+           bottom-height (bottom-height size)
+           top-width (top-width size)
+           top-height (top-height size)}}]
   (hull
    (translate [0 1/2 6]
               (rotate [(to-radians angle) 0 0]
