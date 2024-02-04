8e69407c4576ba47855561f5735f16cdeb4fc9d7
Lars Rustand <rustand.lars@gmail.com>
2024-02-04 14:33:35 +0100

    Add color functions

--- src/my_project/core.clj
+++ src/my_project/core.clj
@@ -4,6 +4,16 @@
   (:use [clojure.math]))
 
 
+(def red (partial color [1 0 0]))
+(def green (partial color [0 1 0]))
+(def blue (partial color [0 0 1]))
+(def purple (partial color [1 0 1]))
+(def black (partial color [0 0 0]))
+(def white (partial color [1 1 1]))
+(def grey (partial color [0.5 0.5 0.5]))
+(def dark-grey (partial color [0.25 0.25 0.25]))
+(def light-grey (partial color [0.75 0.75 0.75 0.4]))
+
 ;; Dimensions are taken from the svg
 (defn place-screws [screw]
   (map (fn [pos]
